20c10b6f7db89a9aeabeea89c8a7fb7553de844e
Masamichi Kagaya <masamichi.kagaya@gmail.com>
2021-11-26 14:26:05 +0900

    Commen out Dev / Contact Links

--- data/nav_links.ts
+++ data/nav_links.ts
@@ -5,7 +5,7 @@ export const nav_links: {
     text: object
 }[] = [
         { key: "0", href: "/", root: "", text: { en: 'Creator', ja: 'Creator' } },
-        { key: "1", href: "/developer", root: "developer", text: { en: 'Developer', ja: 'Developer' } },
-        { key: "2", href: "/contact", root: "contact", text: { en: 'Contact', ja: 'お問い合わせ' } },
+        // { key: "1", href: "/developer", root: "developer", text: { en: 'Developer', ja: 'Developer' } },
+        // { key: "2", href: "/contact", root: "contact", text: { en: 'Contact', ja: 'お問い合わせ' } },
         // { key: "3", href: "/account", root: "account", text: "アカウント" },
     ]
